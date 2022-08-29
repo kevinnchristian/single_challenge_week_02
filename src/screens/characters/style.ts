0daaf89a60d4794a9d6d43fd9849261e67b3e537
kevinnchristian <kevin.cmribeiro@outlook.com>
2022-08-28 23:52:15 -0300

    :construction: feat: implement filter buttons

--- src/screens/characters/style.ts
+++ src/screens/characters/style.ts
@@ -9,6 +9,11 @@ const styles = StyleSheet.create({
     height: '100%',
     width: Dimensions.get('window').width,
   },
+  contentButtonsFilters: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   contentCards: {
     alignSelf: 'center',
   },
@@ -20,7 +25,42 @@ const styles = StyleSheet.create({
     textShadowColor: '#F2D64B',
     textShadowOffset: { width: -2, height: 1.8 },
     textShadowRadius: 4,
-    paddingVertical: 20,
+    paddingVertical: 24,
+  },
+  buttonAll: {
+    backgroundColor: '#F2D64B',
+    borderRadius: 6,
+    color: '#02051b',
+    marginHorizontal: 5,
+    paddingHorizontal: 18,
+    paddingVertical: 7,
+  },
+  buttonFavorites: {
+    backgroundColor: '#05F2AF',
+    borderRadius: 6,
+    color: '#02051b',
+    marginHorizontal: 5,
+    paddingHorizontal: 18,
+    paddingVertical: 7,
+  },
+  buttonClearFavorites: {
+    backgroundColor: '#B65CF2',
+    borderRadius: 6,
+    color: '#02051b',
+    marginHorizontal: 5,
+    paddingHorizontal: 18,
+    paddingVertical: 7,
+  },
+  textButtons: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  textEmpty: {
+    alignSelf: 'center',
+    color: '#E5EAEF',
+    fontSize: 20,
+    fontWeight: 'bold',
+    paddingTop: (Dimensions.get('window').height / 2) - 100,
   }
 });
 
